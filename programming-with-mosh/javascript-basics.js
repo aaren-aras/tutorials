2ab345452dac338ae17b4611ec4501531df3fa57
Aaren Arasaratnam <aaren.aras@gmail.com>
2023-03-11 01:19:38 -0500

    first commit

---
+++ programming-with-mosh/javascript-basics.js
@@ -0,0 +1,15 @@
+// https://www.youtube.com/watch?v=W6NZfCO5SIk&t=2008s 
+// Write ALL notes here for now, neatly format (to upload to GitHub for recruiters), then make a personal copy on obsidian with backlinks, etc. for personal wiki ******
+
+// JavaScript can be used for web/mobile apps, real-time networking apps, command-line tools, and browser-based games!
+// The code runs on browsers (each with JavaScript engine to execute JS code: called "SpiderMonkey" (in FireFox) and "v8" (in Chrome))
+// Fun fact: Node.js is v8 (Google Chrome's open-source JavaScript engine) embedded in a C++ program; therefore, now we can run JS outside of browser (backend)! 
+// Both browsers and Node.js provide a run-time env for JS  
+// ECMAScript is a specification/standard; nowadays we use ES2015/ES6
+
+
+
+
+
+
+
