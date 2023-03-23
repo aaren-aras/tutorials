3e96cbd4dd59d351e3889e9c7df19dc1e4db231d
Aaren Arasaratnam <aaren.aras@gmail.com>
2023-03-23 01:14:17 -0400

    test commit

---
+++ freeCodeCamp/learn-javascript-full-course/index.js
@@ -0,0 +1,15 @@
+// Course: https://www.youtube.com/watch?v=PkZNo7MFNFg&list=PLWKjhJtqVAbleDe3_ZA8h3AO2rXar-q2V&index=5
+
+/* Data Types: 
+  undefined (not given a value yet), 
+  null (given the value "nothing"), 
+  boolean (true/false), 
+  string: 
+  symbol: unique immutable primitive type
+  number:
+  object */
+
+// Variables store and manipulate values in computer's memory in dynamic fashion; storage space/label for your data 
+var myName = "Aaren"
+// Can set to other data types later 
+myName = 8
