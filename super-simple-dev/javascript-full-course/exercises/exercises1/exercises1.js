d97388321cc8d4736fff98ca85308b47b3b18ba7
Aaren Arasaratnam <aaren.aras@gmail.com>
2023-04-01 00:10:18 -0400

    start supersimpledev javascript course

---
+++ super-simple-dev/javascript-full-course/exercises/exercises1/exercises1.js
@@ -0,0 +1,32 @@
+/* Lesson 1 Exercises - JavaScript Basics */
+// **Note: `alert()` doesn't exist in terminal, but rather the `window` object in web browsers
+
+// 1a
+alert("Good morning!");
+
+// 1b
+alert("Aaren");
+
+// 1c
+10 + 5;
+
+// 1d
+20 - 5;
+
+// 1e
+2 + 2 - 5;
+
+// 1f
+document.body.innerHTML = "Good morning!";
+
+// 1g
+document.body.innerHTML = "Aaren";
+
+// 1h
+10 + 8 + 20;
+
+// 1i
+100 - 20 - 50 + 200;
+
+// 1j
+document.body.innerHTML = "";
